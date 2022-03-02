2a0acf12c3f463a4e30c2eb8c3e2a31b7bf3bba3
ahmedelsayed101 <ahmedessayed101@gmail.com>
2022-03-02 12:20:04 +0200

    Add shop app to settings

--- myshop/settings.py
+++ myshop/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #local
+    'shop.apps.ShopConfig',
+    #django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
