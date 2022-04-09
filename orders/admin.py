f0699533f4918b24f1491aa989294a00b147a32c
ahmedelsayed101 <ahmedessayed101@gmail.com>
2022-04-09 15:45:06 +0200

    Add order detail to order admin list display

--- orders/admin.py
+++ orders/admin.py
@@ -1,11 +1,10 @@
-import imp
-from urllib import response
 from django.contrib import admin
 from .models import Order, OrderItem
 import csv
 import datetime
 from django.http import HttpResponse
-
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
 def export_to_csv(modeladmin, request, queryset):
     opts = modeladmin.model._meta
@@ -29,6 +28,11 @@ def export_to_csv(modeladmin, request, queryset):
 
 export_to_csv.short_description = 'Export to CSV'
 
+
+def order_detail(obj):
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+    return mark_safe(f'<a href="{url}">View</a>')
+
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
@@ -37,7 +41,10 @@ class OrderItemInline(admin.TabularInline):
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = [
-        'id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid', 'created', 'updated'
+        'id', 'first_name', 'last_name',
+        'email', 'address', 'postal_code',
+        'city', 'paid', 'created', 'updated',
+        order_detail
     ]
     list_filter = [
         'paid', 'created', 'updated'
