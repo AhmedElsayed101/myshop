bc07206e1b4d771677f7fdb51d34726ba159c16c
ahmedelsayed101 <ahmedessayed101@gmail.com>
2022-04-09 15:59:18 +0200

    Add order_pdf to order admin list display

--- orders/admin.py
+++ orders/admin.py
@@ -33,6 +33,13 @@ def order_detail(obj):
     url = reverse('orders:admin_order_detail', args=[obj.id])
     return mark_safe(f'<a href="{url}">View</a>')
 
+def order_pdf(obj):
+    url = reverse('orders:admin_order_pdf', args=[obj.id])
+    return mark_safe(f'<a href="{url}">PDF</a>')
+
+order_pdf.shor_description = 'Invoice'
+
+
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
@@ -44,7 +51,8 @@ class OrderAdmin(admin.ModelAdmin):
         'id', 'first_name', 'last_name',
         'email', 'address', 'postal_code',
         'city', 'paid', 'created', 'updated',
-        order_detail
+        order_detail,
+        order_pdf
     ]
     list_filter = [
         'paid', 'created', 'updated'
