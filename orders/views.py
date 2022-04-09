b0f2ec5407f8fd8f72a44d8c425fc369cf0e103a
ahmedelsayed101 <ahmedessayed101@gmail.com>
2022-04-09 15:58:39 +0200

    Create admin_order_pdf view

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,4 @@
+from urllib import response
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from .models import OrderItem
@@ -6,7 +7,12 @@ from cart.cart import Cart
 from .tasks import order_created
 from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import get_object_or_404
+from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+import weasyprint
 from .models import Order
+
 
 def order_create(request):
 
@@ -51,3 +57,22 @@ def admin_order_detail(request, order_id):
             'order' : order
         }
     )
+
+@staff_member_required
+def admin_order_pdf(request, order_id):
+    order = get_object_or_404(Order, id = order_id)
+    html = render_to_string(
+        'orders/order/pdf.html',
+        {
+            'order': order
+        }
+    )
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = f'filename=order_{order.id}.pdf'
+    weasyprint.HTML(string=html).write_pdf(
+        response,
+        stylesheets=[weasyprint.CSS(
+            settings.STATIC_ROOT + 'css/pdf.css'
+        )]
+    )
+    return response
