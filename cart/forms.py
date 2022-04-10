4e41662b71e0d1632c25f6b51004f88727a7196b
ahmedelsayed101 <ahmedessayed101@gmail.com>
2022-04-10 21:02:19 +0200

    Add trans to all scripts

--- cart/forms.py
+++ cart/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-
+from django.utils.translation import gettext_lazy as _
 
 PRODCUT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 
@@ -7,7 +7,8 @@ class CartAddProductForm(forms.Form):
 
     quantity = forms.TypedChoiceField(
         choices=PRODCUT_QUANTITY_CHOICES,
-        coerce=int
+        coerce=int,
+        label=_('Quantity')
     )
     override = forms.BooleanField(
         required=False,
